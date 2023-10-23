48d496e62df2d6feec2484c722ade71ca592450d
qiross <lucasqreis@poli.ufrj.br>
2023-10-23 16:59:32 -0300

    Used just to practive url testing for another project

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class URLTests(TestCase):
+    def test_home_url(self):
+        self.assertEqual(1,1)
+        response = self.client.get(reverse('charts'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_other_url(self):
+        response = self.client.get(reverse('charts'))
+        self.assertTemplateUsed(response, 'blog/charts.html')
+        # response = self.client.get(reverse(''))
+        # self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, 'Expected Content')
